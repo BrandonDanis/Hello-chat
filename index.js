ca2de0f485060f50e57ca32d6aeea899629a6b0c
Brandon Danis <brandon@spottedu.co>
2016-01-29 17:14:44 -0500

    Started on /app

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ console.log("App is now listening on port 8080".green);
 console.log("#################################".green);
 
 //routing
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/app'));
 
 var users = 0;
 
