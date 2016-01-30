66dd9e0cd8adfbc1e384c33f244ec7e430c96d25
Brandon Danis <brandon@spottedu.co>
2016-01-29 19:59:33 -0500

    Saving.

--- app/js/index.js
+++ app/js/index.js
@@ -22,6 +22,16 @@ $(function() {
 
 	socket.on('new user', function(res) {
 		console.log("New user! " + res.username);
+
+		var notification = '<div class="alert alert-warning alert-dismissible fade in" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button> <strong>New User!</strong> ~USERNAME~ </div>';
+		notification = notification.replace(/~USERNAME~/g, res.username);
+
+		$('.loginSection').append(notification);
+
+		setTimeout(function() {
+			$('.loginSection .alert').remove();
+		}, 2000);
+
 	});
 
 });
